b55a0122d77c93ed542418295003bfa64de13c84
romilshah803 <74334375+romilshah803@users.noreply.github.com>
2021-03-02 21:53:40 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+var bg;
+function preload(){
+    bg=loadImage("images/board.png");
+}
+
+
+function setup(){
+    var canvas = createCanvas(800,800);
+    
+}
+
+function draw(){
+    background(bg);
+    
+}
